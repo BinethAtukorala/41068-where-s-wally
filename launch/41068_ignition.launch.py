0b9d5561ee9d03595bc0f44a921e4d2828f1d90b
Bineth Atukorala <bineth.mandiv@gmail.com>
2025-08-18 14:00:26 +1000

    Added SJTU Drone

--- launch/41068_ignition.launch.py
+++ launch/41068_ignition.launch.py
@@ -7,6 +7,9 @@ from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
 from launch_ros.substitutions import FindPackageShare
 
+import xacro
+import yaml
+import os
 
 def generate_launch_description():
 
@@ -39,19 +42,80 @@ def generate_launch_description():
     ld.add_action(nav2_launch_arg)
 
     # Load robot_description and start robot_state_publisher
-    robot_description_content = ParameterValue(
-        Command(['xacro ',
-                 PathJoinSubstitution([pkg_path,
-                                       'urdf',
-                                       'husky.urdf.xacro'])]),
-        value_type=str)
-    robot_state_publisher_node = Node(package='robot_state_publisher',
-                                      executable='robot_state_publisher',
-                                      parameters=[{
-                                          'robot_description': robot_description_content,
-                                          'use_sim_time': use_sim_time
-                                      }])
-    ld.add_action(robot_state_publisher_node)
+    # robot_description_content = ParameterValue(
+    #     Command(['xacro ',
+    #              PathJoinSubstitution([pkg_path,
+    #                                    'urdf',
+    #                                    'husky.urdf.xacro'])]),
+    #     value_type=str)
+    # robot_state_publisher_node = Node(package='robot_state_publisher',
+    #                                   executable='robot_state_publisher',
+    #                                   parameters=[{
+    #                                       'robot_description': robot_description_content,
+    #                                       'use_sim_time': use_sim_time
+    #                                   }])
+    # ld.add_action(robot_state_publisher_node)
+
+    # Quadcopter - Load drone_description and start drone_state_publisher 
+    # drone_description_content = ParameterValue(
+    #     Command(['xacro ', 
+    #              PathJoinSubstitution([FindPackageShare('sjtu_drone_description'),
+    #                                    'urdf',
+    #                                    'sjtu_drone.urdf.xacro'])]),
+    #     value_type=str
+    # )
+
+    # drone_yaml_file_path = PathJoinSubstitution([
+    #     FindPackageShare('sjtu_drone_description'),
+    #     'config', 'drone.yaml'
+    # ])   
+
+    # drone_state_publisher_node = Node(package='drone_state_publisher',
+    #                                   executable='drone_state_publisher',
+    #                                   parameters=[{
+    #                                       'robot_description': drone_description_content,
+    #                                       'use_sim_time': use_sim_time
+    #                                   }],
+    #                                   remappings=[('/robot_description', '/drone_description')]
+    #                                   )
+    # ld.add_action(drone_state_publisher_node)
+
+    drone_xacro_file_name = "sjtu_drone.urdf.xacro"
+    drone_xacro_file = os.path.join(
+        FindPackageShare('sjtu_drone_description').find('sjtu_drone_description'),
+        "urdf", 
+        drone_xacro_file_name
+    )
+
+    drone_yaml_file_path = os.path.join(
+        FindPackageShare('41068_ignition_bringup').find('41068_ignition_bringup'),
+        "config",
+        "drone.yaml"
+    )
+
+    drone_description_config = xacro.process_file(str(drone_xacro_file), mappings={"params_path": str(drone_yaml_file_path)})
+    drone_desc = drone_description_config.toxml()
+
+    # get ns from yaml
+    with open(drone_yaml_file_path, 'r') as f:
+        yaml_dict = yaml.load(f, Loader=yaml.FullLoader)
+        model_ns = yaml_dict["namespace"]
+    print("namespace: ", model_ns)
+
+    drone_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        namespace=model_ns,
+        parameters=[{
+            "use_sim_time": use_sim_time,
+            "robot_description": drone_desc,
+            "frame_prefix": model_ns + "/"
+        }],
+        arguments=[drone_desc]
+    )
+
+    ld.add_action(drone_state_publisher_node)
 
     # Publish odom -> base_link transform **using robot_localization**
     robot_localization_node = Node(
@@ -85,14 +149,26 @@ def generate_launch_description():
     ld.add_action(gazebo)
 
     # Spawn robot in Gazebo
-    robot_spawner = Node(
+    # robot_spawner = Node(
+    #     package='ros_ign_gazebo',
+    #     executable='create',
+    #     output='screen',
+    #     parameters=[{'use_sim_time': use_sim_time}],
+    #     arguments=['-topic', '/robot_description', '-z', '0.4']
+    # )
+    # ld.add_action(robot_spawner)
+
+    # Spawn drone in Gazebo
+    drone_spawner = Node(
         package='ros_ign_gazebo',
         executable='create',
         output='screen',
         parameters=[{'use_sim_time': use_sim_time}],
-        arguments=['-topic', '/robot_description', '-z', '0.4']
+        arguments=['-topic', '/robot_description',
+                   '-y', '-1',
+                   '-z', '0.4']
     )
-    ld.add_action(robot_spawner)
+    ld.add_action(drone_spawner)
 
     # Bridge topics between gazebo and ROS2
     gazebo_bridge = Node(
